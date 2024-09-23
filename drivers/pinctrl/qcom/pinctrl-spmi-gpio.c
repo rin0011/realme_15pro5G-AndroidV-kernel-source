52cfb3c78a0d2598f1060c3c4d31a90190782cc8
Shilpa Suresh <quic_c_sbsure@quicinc.com>
2024-09-23 23:15:28 +0530

    pinctrl: qcom: spmi-gpio: add support for PM7325B
    Add compatible string for 'qcom,pm7325b-gpio'.
    
    Change-Id: I89238d08578d7ee793856bba912fa7323e16d451
    Signed-off-by: Fenglin Wu <quic_fenglinw@quicinc.com>
    Signed-off-by: Shilpa Suresh <quic_c_sbsure@quicinc.com>

--- drivers/pinctrl/qcom/pinctrl-spmi-gpio.c
+++ drivers/pinctrl/qcom/pinctrl-spmi-gpio.c
@@ -1238,6 +1238,7 @@ static const struct of_device_id pmic_gpio_of_match[] = {
 	{ .compatible = "qcom,pm6450-gpio", .data = (void *) 9 },
 	{ .compatible = "qcom,pm7250b-gpio", .data = (void *) 12 },
 	{ .compatible = "qcom,pm7325-gpio", .data = (void *) 10 },
+	{ .compatible = "qcom,pm7325b-gpio", .data = (void *) 8 },
 	{ .compatible = "qcom,pm7550ba-gpio", .data = (void *) 8},
 	{ .compatible = "qcom,pm8005-gpio", .data = (void *) 4 },
 	{ .compatible = "qcom,pm8008-gpio", .data = (void *) 2 },
