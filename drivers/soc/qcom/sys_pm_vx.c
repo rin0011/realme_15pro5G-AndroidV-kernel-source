35c9b27c47c5b254256a5d9d3a9adef10acad8e2
Sneh Mankad <quic_smankad@quicinc.com>
2024-09-24 14:27:04 +0530

    drivers: soc: qcom: Add sys-pm-violators list for tuna
    This change adds subsystem DRVs for tuna to be monitored for
    sys-pm violations.
    
    Change-Id: I3d547058a4cc79fb62ae5aa5a99188828207ec24
    Signed-off-by: Sneh Mankad <quic_smankad@quicinc.com>

--- drivers/soc/qcom/sys_pm_vx.c
+++ drivers/soc/qcom/sys_pm_vx.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2024, Qualcomm Innovation Center, Inc. All rights reserved.
  *
  */
 
@@ -134,6 +134,16 @@ static const char * const drv_names_sun[][MAX_DRV_NAMES] = {
 			"DDR AUX", "ARC CPRF", ""},
 	[AOSS_DRV_NAME] = {"APPS", "SP", "AUDIO", "AOP", "DEBUG", "GPU", "DISPLAY", "COMPUTE",
 			"TME", "MODEM", "WLAN BB", "CAM", "PCIE", "MM", ""},
+};
+
+static const char * const drv_names_tuna[][MAX_DRV_NAMES] = {
+	[CXPC_DRV_NAME] = {"TZ", "L3", "HLOS", "HYP", "WPSS", "AUDIO", "AUDIO CESTA", "AOP",
+			"DEBUG", "GPU", "DISPLAY", "COMPUTE_DSP", "TME_HW", "TME_SW", "MDM SW",
+			"MDM HW", "MDM Q6 CESTA", "WLAN RF", "WLAN BB", "CAM_IFE0 CESTA",
+			"CAM_IFE1", "CAM_IFE2", "PCI0 CESTA", "MM CESTA",
+			"DDR AUX", "ARC CPRF", ""},
+	[AOSS_DRV_NAME] = {"APPS", "WPSS", "AUDIO", "AOP", "DEBUG", "GPU", "DISPLAY", "COMPUTE",
+			"TME", "MODEM", "WLAN RF", "WLAN BB", "CAM", "PCIE", "MM", ""},
 };
 
 static ssize_t debug_time_ms_show(struct device *dev,
@@ -540,6 +550,8 @@ static const struct of_device_id drv_match_table[] = {
 	  .data = drv_names_pineapple },
 	{ .compatible = "qcom,sys-pm-sun",
 	  .data = drv_names_sun },
+	{ .compatible = "qcom,sys-pm-tuna",
+	  .data = drv_names_tuna },
 	{ }
 };
 
