36b38313315f552ed255c9f120267c005125027f
Krishna Kurapati <quic_kriskura@quicinc.com>
2024-10-29 10:14:45 +0530

    usb: dwc3: msm: Check explicitly if dwc3 core is suspended or not
    While exiting peripheral mode, it is better to explicitly check if we
    are suspended or not instead of checking for return value. Whatever
    be the reason, put sync on child is ideally supposed to suspend the
    child and ensure we are good to suspend the wrapper. There is a chance
    that put sync might return 0 although the disconnect interrupt didn't
    come and still the child is not suspended. So check explicitly whether
    the child is suspended or not before we try/skip force suspending it.
    
    Change-Id: Ic9dcabc3f405f4bc4093bbb403410eedc0564361
    Signed-off-by: Krishna Kurapati <quic_kriskura@quicinc.com>

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -7089,7 +7089,7 @@ static int dwc3_otg_start_peripheral(struct dwc3_msm *mdwc, int on)
 		 * or pullup disable), and retry suspend again.
 		 */
 		ret = pm_runtime_put_sync(&mdwc->dwc3->dev);
-		if (ret == -EBUSY) {
+		if (!pm_runtime_suspended(&mdwc->dwc3->dev)) {
 			while (--timeout && dwc->connected)
 				msleep(20);
 			dbg_event(0xFF, "StopGdgt connected", dwc->connected);
