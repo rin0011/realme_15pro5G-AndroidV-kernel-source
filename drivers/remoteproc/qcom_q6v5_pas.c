d7ebbfdfb1475f0bbc92108159de2b5188af87e3
Wasim Nazir <quic_wasimn@quicinc.com>
2024-12-28 16:46:15 +0530

    remoteproc: qcom_q6v5: Enable auto boot in kera
    Enable auto boot for adsp/cdsp in kera.
    
    Change-Id: I0379f690475296707e222a44d4c60a299dd4a91e
    Signed-off-by: Wasim Nazir <quic_wasimn@quicinc.com>

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -2112,7 +2112,7 @@ static const struct adsp_data kera_adsp_resource = {
 	.sysmon_name = "adsp",
 	.ssctl_id = 0x14,
 	.uses_elf64 = true,
-	.auto_boot = false,
+	.auto_boot = true,
 	.crash_reason_stack = 660,
 	.smem_host_id = 2,
 };
@@ -2133,7 +2133,7 @@ static const struct adsp_data kera_cdsp_resource = {
 	.region_assign_count = 1,
 	.region_assign_shared = true,
 	.region_assign_vmid = QCOM_SCM_VMID_CDSP,
-	.auto_boot = false,
+	.auto_boot = true,
 	.crash_reason_stack = 660,
 	.smem_host_id = 5,
 };
