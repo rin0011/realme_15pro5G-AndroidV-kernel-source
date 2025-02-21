4184925472f5643db8ccc485ec740ff9746d406c
Yuanfang Zhang <quic_yuanfang@quicinc.com>
2025-02-21 14:16:57 +0800

    memory_dump_v2: Fix Use-After-Free issue
    Prohibit disabling dump entries that have already been enabled.
    
    Change-Id: I96c66358f97a3062083e1bc6c27f047b717dcbba
    Signed-off-by: Yuanfang Zhang <quic_yuanfang@quicinc.com>

--- drivers/soc/qcom/memory_dump_v2.c
+++ drivers/soc/qcom/memory_dump_v2.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2014-2017, 2019-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/slab.h>
@@ -1180,7 +1180,7 @@ static void mem_dump_free_rmem(phys_addr_t base, uint32_t size)
 static int dynamic_mem_dump_disable(struct memdump_info *dump_info)
 {
 	mutex_lock(&dump_info->mutex);
-	if (!dump_info->active) {
+	if (dump_info->enable || !dump_info->active) {
 		mutex_unlock(&dump_info->mutex);
 		return 0;
 	}
